d73620efa4766e7fa2d30f36189a263fe1abd62f
vanny <vdevelopper@gmail.com>
2022-08-03 18:19:42 +0200

    creation de screen de scan QrCode

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -18,100 +18,106 @@ export default function LoginScreen() {
         }
 
         return (
-                <View style={{ flex: 1 }}>
-                        <ImageBackground imageStyle={{ flex: 1, resizeMode: "contain", opacity: 0.5, transform: [{ scale: 2 }], alignSelf: "center", height: "100%" }} source={require('../../../assets/images/home3.png')}>
-                                <ScrollView keyboardShouldPersistTaps="handled">
-                                        <View style={styles.container}>
-                                                <Image source={require('../../../assets/images/Mediabox.png')} style={{ ...styles.image, resizeMode: "center", height: (10 * height - StatusBar.currentHeight) / 100 }} />
-                                                <Text style={styles.title}>Connexion</Text>
-                                                <Input
-                                                        placeholder='Email ou numéro de téléphone'
-                                                        InputLeftElement={
-                                                                <Icon
-                                                                        as={<MaterialIcons name="alternate-email" size={20} color="#777" />}
-                                                                        size={6}
-                                                                        mr="2"
-                                                                        color="muted.400"
-                                                                />
-                                                        }
-                                                        py={2}
-                                                        onChangeText={(em) => setEmail(em)}
-                                                        value={email}
-                                                        _focus={{
-                                                                borderBottomColor: primaryColor
-                                                        }}
-                                                        returnKeyType="next"
-                                                        blurOnSubmit={false}
-                                                        onSubmitEditing={() => {
-                                                                passwordInputRef.current.focus()
-                                                        }}
-                                                        autoCompleteType='off'
-                                                />
+                <View style={{ flex: 1, backgroundColor: "#E6E3EA" }}>
+                        {/* <ImageBackground imageStyle={{ flex: 1, resizeMode: "contain", opacity: 0.5, transform: [{ scale: 2 }], alignSelf: "center", height: "100%" }} source={require('../../../assets/images/home3.png')}> */}
+                        <ScrollView keyboardShouldPersistTaps="handled">
+                                <View style={styles.container}>
+                                        <Image source={require('../../../assets/images/Mediabox.png')} style={{ ...styles.image, resizeMode: "center", height: (20 * height - StatusBar.currentHeight) / 100 }} />
+                                        <Text style={styles.title}>Connexion</Text>
+                                        <Input
+                                                placeholder='Email ou numéro de téléphone'
+                                                InputLeftElement={
+                                                        <Icon
+                                                                as={<MaterialIcons name="alternate-email" size={20} color="#777" />}
+                                                                size={6}
+                                                                mr="2"
+                                                                color="muted.400"
+                                                        />
+                                                }
+                                                py={2}
+                                                borderWidth={0}
+                                                borderBottomWidth={1}
+                                                borderBottomColor={'#F58424'}
+                                                onChangeText={(em) => setEmail(em)}
+                                                value={email}
+                                                _focus={{
+                                                        borderBottomColor: primaryColor
+                                                }}
+                                                returnKeyType="next"
+                                                blurOnSubmit={false}
+                                                onSubmitEditing={() => {
+                                                        passwordInputRef.current.focus()
+                                                }}
+                                                autoCompleteType='off'
+                                        />
 
-                                                <Input
-                                                        placeholder='Mot de passe'
-                                                        InputLeftElement={
-                                                                <Icon
-                                                                        as={<Ionicons name="lock-closed-outline" size={20} color="#777" />}
-                                                                        size={6}
-                                                                        mr="2"
-                                                                        color="muted.400"
-                                                                />}
-                                                        InputRightElement={
-                                                                <Icon
-                                                                        as={<Ionicons name={!showPassword ? "eye-off-outline" : "eye-outline"} size={20} color="#777" />}
-                                                                        size={6}
-                                                                        mr="2"
-                                                                        color="muted.400"
-                                                                        onPress={() => setShowPassword(t => !t)}
-                                                                />}
-                                                        secureTextEntry={!showPassword}
-                                                        onChangeText={(em) => setPassword(em)}
-                                                        value={password}
-                                                        py={2}
-                                                        _focus={{
-                                                                borderBottomColor: primaryColor
-                                                        }}
-                                                        returnKeyType="go"
-                                                        mt={3}
-                                                        ref={passwordInputRef}
-                                                />
-                                                <TouchableOpacity>
-                                                        <Text style={styles.forgetPass}>Mot de passe oublié</Text>
-                                                </TouchableOpacity>
+                                        <Input
+                                                placeholder='Mot de passe'
+                                                InputLeftElement={
+                                                        <Icon
+                                                                as={<Ionicons name="lock-closed-outline" size={20} color="#777" />}
+                                                                size={6}
+                                                                mr="2"
+                                                                color="muted.400"
+                                                        />}
+                                                InputRightElement={
+                                                        <Icon
+                                                                as={<Ionicons name={!showPassword ? "eye-off-outline" : "eye-outline"} size={20} color="#777" />}
+                                                                size={6}
+                                                                mr="2"
+                                                                color="muted.400"
+                                                                onPress={() => setShowPassword(t => !t)}
+                                                        />}
+                                                secureTextEntry={!showPassword}
+                                                onChangeText={(em) => setPassword(em)}
+                                                value={password}
+                                                py={2}
+                                                borderWidth={0}
+                                                borderBottomWidth={1}
+                                                borderBottomColor={'#F58424'}
+                                                _focus={{
+                                                        borderBottomColor: primaryColor
+                                                }}
+                                                returnKeyType="go"
+                                                mt={3}
+                                                ref={passwordInputRef}
+                                        />
+                                        <TouchableOpacity>
+                                                <Text style={styles.forgetPass}>Mot de passe oublié</Text>
+                                        </TouchableOpacity>
 
-                                                <Button
-                                                        borderRadius={15}
-                                                        // isDisabled={email == "" || password == ""}
-                                                        onPress={handleLogin}
-                                                        mt={5}
-                                                        backgroundColor={"#F58424"}
-                                                        py={3.5}
-                                                        _text={{ color: '#fff', fontWeight: 'bold' }}
-                                                >
-                                                        Se connecter
-                                                </Button>
+                                        <Button
+                                                borderRadius={15}
+                                                // isDisabled={email == "" || password == ""}
+                                                onPress={handleLogin}
+                                                mt={5}
+                                                backgroundColor={"#F58424"}
+                                                py={3.5}
+                                                _text={{ color: '#fff', fontWeight: 'bold' }}
+                                        >
+                                                Se connecter
+                                        </Button>
 
-                                                <Button
-                                                        //onPress={() => navigation.navigate('Registre')}
-                                                        borderRadius={15}
-                                                        mt={5}
-                                                        px={12}
-                                                        py={3}
-                                                        size="lg"
-                                                        backgroundColor={"transparent"}
-                                                        borderColor={"#F58424"}
-                                                        borderWidth={1}
-                                                        _text={{
-                                                                fontWeight: 'bold',
-                                                                color: "#F58424"
-                                                        }}
-                                                >
-                                                        S'inscrire
-                                                </Button>
-                                        </View>
-                                </ScrollView>
-                        </ImageBackground>
+                                        <Button
+                                                //onPress={() => navigation.navigate('Registre')}
+                                                borderRadius={15}
+                                                mt={5}
+                                                px={12}
+                                                py={3}
+                                                size="lg"
+                                                backgroundColor={"transparent"}
+                                                borderColor={"#F58424"}
+                                                borderWidth={1}
+                                                _text={{
+                                                        fontWeight: 'bold',
+                                                        color: "#F58424"
+                                                }}
+                                        >
+                                                S'inscrire
+                                        </Button>
+                                </View>
+                        </ScrollView>
+                        {/* </ImageBackground> */}
                 </View>
         )
 }
--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,10 +1,86 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StatusBar, StyleSheet, useWindowDimensions, Image } from "react-native";
+import { Button, Icon, Input } from 'native-base'
+import { EvilIcons, AntDesign } from '@expo/vector-icons';
 
-export default function HomeScreen(){
-        return(
-                <View>
-                        <Text>Home accueil</Text>
+export default function HomeScreen() {
+        const { height } = useWindowDimensions()
+        return (
+                <View style={{ flex: 1, backgroundColor: "#E6E3EA" }}>
+                        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+                        <View style={styles.header}>
+                                {/* <Image source={require('../../../assets/images/home.png')} style={{ ...styles.image, resizeMode: "center", height: (20 * height - StatusBar.currentHeight) / 100 }}/> */}
+                                <Text style={styles.titlePrincipal}>Bienvenue sur l'application  </Text>
+                                <Text style={styles.titleSecondaire}>de verification de </Text>
+                                <Text style={styles.titleAccessoire}> demande de rendez-vous </Text>
+                        </View>
+
+                        <View style={styles.footer}>
+                                <Text style={styles.textQrcode}>Scaner le Qr Code du Certidicat</Text>
+                        </View>
+                        <Button
+                                borderRadius={15}
+                                //     onPress={()=>navigation.navigate('Scan')}
+                                mt={7}
+                                backgroundColor={"#F58424"}
+                                py={3.5}
+                                marginHorizontal={80}
+                                leftIcon={
+                                        <Icon
+                                                as={<Icon as={AntDesign} name="scan1" size={'sm'} color="#fff" />}
+                                                size={8}
+                                                ml="2"
+                                                color="#369c69"
+                                                fontWeight={"bold"}
+                                        />
+                                }
+                                _text={{ color: '#fff', fontWeight: 'bold', fontSize: 20 }}
+                        >
+                                Scan
+                        </Button>
+
                 </View>
         )
 }
+
+const styles = StyleSheet.create({
+        header: {
+                height: "45%",
+                marginTop: 20,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#ddd",
+                marginHorizontal: 10,
+                borderRadius: 20,
+                borderWidth: 3,
+                borderColor: "#fff"
+        },
+        footer: {
+                marginHorizontal: 10,
+                marginTop: 30,
+                justifyContent: "center",
+                alignItems: "center",
+
+        },
+        image: {
+                Width: '100%',
+                alignSelf: 'center',
+                marginTop: 30
+        },
+        titlePrincipal: {
+                fontSize: 25,
+                fontWeight: "bold"
+        },
+        titleSecondaire: {
+                fontSize: 25,
+                fontWeight: "bold"
+        },
+        titleAccessoire: {
+                fontSize: 25,
+                fontWeight: "bold"
+        },
+        textQrcode: {
+                fontSize: 20,
+                color: "#777",
+        }
+})
