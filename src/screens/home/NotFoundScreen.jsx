810bd9f9d9e237cc04f0d10156f25927a8d761f6
vanny <vdevelopper@gmail.com>
2022-08-08 16:25:52 +0200

    ajout de screen de NotFound

--- src/screens/home/NotFoundScreen.jsx
+++ src/screens/home/NotFoundScreen.jsx
@@ -3,7 +3,7 @@ import { View, Text, TouchableNativeFeedback, Image, StyleSheet} from 'react-nat
 import NotFound from "../../components/NotFound";
 import {Ionicons} from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
-
+``
 export default function NotFoundScreen(){
         const navigation = useNavigation()
         return(
