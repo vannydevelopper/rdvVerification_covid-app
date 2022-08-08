87634fec313b7a3eb80da5ade2562952052c9824
vanny <vdevelopper@gmail.com>
2022-08-08 12:14:39 +0200

    ajout de trost et de loading

--- src/screens/home/PhotoScreen.jsx
+++ src/screens/home/PhotoScreen.jsx
@@ -2,8 +2,11 @@ import React, { useState, useEffect, useRef } from 'react'
 import { View, Text, StyleSheet, useWindowDimensions, TouchableOpacity, ScrollView, ImageBackground, TouchableNativeFeedback, Image, TouchableWithoutFeedback, ActivityIndicator, BackHandler } from 'react-native'
 import * as ImagePicker from 'expo-image-picker';
 import { MaterialIcons, Ionicons, FontAwesome5, AntDesign, FontAwesome, Entypo, Foundation } from '@expo/vector-icons';
-import { Button, Icon, Input, FormControl, WarningOutlineIcon } from 'native-base'
+import { Button, Icon, Input, useToast } from 'native-base';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import * as Location from 'expo-location'
+import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
+import fetchApi from '../../helpers/fetchApi'
 import moment from 'moment'
 moment.updateLocale('fr', {
         calendar: {
@@ -21,9 +24,161 @@ export default function PhotoScreen() {
         const photoTypeSelectRef = useRef(null)
         const navigation = useNavigation()
         const route = useRoute()
+        const [location, setLocation] = useState(null)
+        const toast = useToast()
+        const [isLoading, setIsLoading] = useState(false)
 
         const { donnees, ID_RDV } = route.params
-        console.log(donnees, ID_RDV)
+        //console.log(donnees, ID_RDV)
+
+        const askLocationPermission = async () => {
+                let {
+                        status: locationStatus,
+                } = await Location.requestForegroundPermissionsAsync()
+                if (locationStatus !== 'granted') {
+                        console.log('Permission to access location was denied')
+                        setLocation(false)
+                        return
+                }
+                var location = await Location.getCurrentPositionAsync({})
+                setLocation(location)
+        }
+
+        useEffect(() => {
+                askLocationPermission()
+        }, [])
+
+        if (location === false) {
+                return (
+                        <View
+                                style={{
+                                        alignContent: 'center',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        flex: 1,
+                                }}
+                        >
+                                <Text style={{ fontWeight: 'bold', fontSize: 16, opacity: 0.8 }}>
+                                        Pas d'accès à la localisation
+                                </Text>
+                                <Text
+                                        style={{
+                                                textAlign: 'center',
+                                                color: '#777',
+                                                marginTop: 10,
+                                                paddingHorizontal: 10,
+                                        }}
+                                >
+                                        L'application a besoin de votre localisation pour fonctionner
+                                </Text>
+                                <TouchableNativeFeedback
+                                        background={TouchableNativeFeedback.Ripple('#ddd')}
+                                        useForeground={true}
+                                        onPress={() => askLocationPermission()}
+                                >
+                                        <View
+                                                style={{
+                                                        backgroundColor: '#fff',
+                                                        borderRadius: 10,
+                                                        padding: 10,
+                                                        marginTop: 20,
+                                                }}
+                                        >
+                                                <Text>Autoriser l'accès</Text>
+                                        </View>
+                                </TouchableNativeFeedback>
+                        </View>
+                )
+        }
+
+        const sendData = async () => {
+                setIsLoading(true)
+                if (!location) {
+                        let { status: locationStatus } = await Location.requestForegroundPermissionsAsync();
+                        if (locationStatus !== 'granted') {
+                                return setLoading(false)
+                        }
+                        var loc = await Location.getCurrentPositionAsync({});
+                        setLocation(loc)
+                }
+
+                const form = new FormData()
+                form.append('ID_RDV', ID_RDV)
+                form.append('LONGITUDE', location.coords.longitude)
+                form.append('LATITUDE', location.coords.latitude)
+
+                if (imagebordereaux) {
+                        const manipResult = await manipulateAsync(
+                                imagebordereaux.uri,
+                                [
+                                        // { resize: { width: 300 } }
+                                ],
+                                { compress: 0.7, format: SaveFormat.JPEG }
+                        );
+                        let localUri = manipResult.uri;
+                        let filename = localUri.split('/').pop();
+                        let match = /\.(\w+)$/.exec(filename);
+                        let type = match ? `image/${match[1]}` : `image`;
+                        form.append('PHOTO_BRD', {
+                                uri: localUri, name: filename, type
+                        })
+
+                }
+
+                if (imagepassport) {
+                        const manipResult = await manipulateAsync(
+                                imagepassport.uri,
+                                [
+                                        // { resize: { width: 300 } }
+                                ],
+                                { compress: 0.7, format: SaveFormat.JPEG }
+                        );
+                        let localUri = manipResult.uri;
+                        let filename = localUri.split('/').pop();
+                        let match = /\.(\w+)$/.exec(filename);
+                        let type = match ? `image/${match[1]}` : `image`;
+                        form.append('PHOTO_PRS', {
+                                uri: localUri, name: filename, type
+                        })
+
+                }
+
+                //console.log(form)
+
+                try {
+                        const data = await fetchApi("/historique/ajouter", {
+                                method: "POST",
+                                body: form
+
+                        })
+                        setIsLoading(false)
+                        toast.show({
+                                title: "L'enregistrement est faite avec succes",
+                                placement: "bottom",
+                                status: 'success',
+                                duration: 2000,
+                                width: '90%',
+                                minWidth: 300
+                        })
+                }
+                catch (error) {
+                        console.log(error)
+                        toast.show({
+                                title: "La connexion a echoue",
+                                placement: "bottom",
+                                status: 'success',
+                                duration: 2000,
+                                width: '90%',
+                                minWidth: 300
+                        })
+                        setIsLoading(false)
+                }
+                setIsLoading(false)
+
+
+        }
+
+
 
 
         const onTakePictureSelect = async () => {
@@ -75,7 +230,7 @@ export default function PhotoScreen() {
                         </View>
                         <ScrollView keyboardShouldPersistTaps={"handled"}>
 
-                                { donnees.payement && <View style={styles.cardPrincipal}>
+                                {donnees.payement && <View style={styles.cardPrincipal}>
                                         <View style={styles.titleDetails}>
                                                 <Text style={{ fontSize: 15, fontWeight: "bold" }}>{donnees.message}</Text>
                                         </View>
@@ -168,7 +323,7 @@ export default function PhotoScreen() {
 
                                         <View style={{ flexDirection: "row", alignContent: "center", alignItems: "center", marginTop: 5 }}>
                                                 <View style={styles.cardImage}>
-                                                <AntDesign name="calendar" size={24} color="#F58424" />
+                                                        <AntDesign name="calendar" size={24} color="#F58424" />
 
                                                 </View>
                                                 <View style={{ marginLeft: 13 }}>
@@ -188,7 +343,7 @@ export default function PhotoScreen() {
 
                                         <View style={{ flexDirection: "row", alignContent: "center", alignItems: "center", marginTop: 5 }}>
                                                 <View style={styles.cardImage}>
-                                                <FontAwesome5 name="user" size={24} color="#F58424" />
+                                                        <FontAwesome5 name="user" size={24} color="#F58424" />
 
                                                 </View>
                                                 <View style={{ marginLeft: 13 }}>
@@ -209,7 +364,7 @@ export default function PhotoScreen() {
                                         </View>
                                         <View style={{ flexDirection: "row", alignContent: "center", alignItems: "center", marginTop: 5 }}>
                                                 <View style={styles.cardImage}>
-                                                <AntDesign name="calendar" size={24} color="#F58424" />
+                                                        <AntDesign name="calendar" size={24} color="#F58424" />
 
                                                 </View>
                                                 <View style={{ marginLeft: 13 }}>
@@ -248,8 +403,8 @@ export default function PhotoScreen() {
 
                                                 </View>
                                         </View>
-                                        
-                                       { ! donnees.payement && <View>
+
+                                        {!donnees.payement && <View>
                                                 <Text style={{ paddingHorizontal: 50, color: '#777', fontWeight: 'bold', marginTop: 10 }}>
                                                         Bordereaux
                                                 </Text>
@@ -274,7 +429,8 @@ export default function PhotoScreen() {
                         <Button
                                 borderRadius={15}
                                 // isDisabled={email == "" || password == ""}
-                                //onPress={handleLogin}
+                                isLoading={isLoading}
+                                onPress={sendData}
                                 marginHorizontal={20}
                                 mt={5}
                                 backgroundColor={"#F58424"}
